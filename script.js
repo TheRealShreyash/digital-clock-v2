a88a0da61bfd9e87c4927c50e4d6014d83d0ad63
TheRealShreyash <koshta.shreyash02@gmail.com>
2025-01-02 00:24:15 +0530

    v2 release

---
+++ script.js
@@ -0,0 +1,50 @@
+const time = document.querySelector(".timestring");
+const date = document.querySelector(".date");
+const meridian = document.querySelector(".meridian");
+
+const currentDate = new Date();
+
+const day = currentDate.getDate();
+const month = currentDate.toLocaleString("default", { month: "long" });
+const year = currentDate.getFullYear();
+const weekday = currentDate.toLocaleString("default", { weekday: "long" });
+
+function getOrdinalSuffix(day) {
+  if (day > 3 && day < 21) return "th"; // Covers 11th to 19th
+  switch (day % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+}
+
+const formattedDate = `${day}${getOrdinalSuffix(
+  day
+)} ${month} ${year}, ${weekday}`;
+
+date.innerHTML = `${formattedDate}`;
+
+const updateTime = () => {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const timeString = now.toLocaleTimeString("default", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+  const meridian = hours >= 12 ? "PM" : "AM";
+
+  meridian.innerHTML = meridian;
+  time.innerHTML = timeString.slice(0, 5);
+};
+
+setInterval(() => {
+  updateTime();
+}, 1000);
+
+updateTime();
